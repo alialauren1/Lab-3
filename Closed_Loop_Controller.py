e0bdc83793c20204b7506d9bfa2733581d9852c8
andyjwaideh <andyjwaideh@gmail.com>
2024-02-21 20:43:58 -0800

    Updated Code

--- Closed_Loop_Controller.py
+++ Closed_Loop_Controller.py
@@ -1,4 +1,16 @@
-import time
-import pyb
-
-
+class Controller:
+    def __init__(self, kp, set_point):
+         self.kp = float(kp)
+         self.set_point = float(set_point)
+         
+    def run(self, measured_output):
+         #actuation value is Kp*(theta_set - enc2.read() value
+         actuation = self.kp*(self.set_point - measured_output)
+         return actuation
+        
+    def set_setpoint(self, desired_set_point):
+        self.set_point = desired_set_point
+        
+    def set_Kp(self, desired_Kp):
+        self.kp = desired_Kp
+    
--- main.py
+++ main.py
@@ -7,35 +7,29 @@ from Closed_Loop_Controller import Controller
 
 enc2 = Encoder("enc2", pyb.Pin.board.PC6, pyb.Pin.board.PC7, 8)
 moe = motordriver (pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
-enc.zero()
+enc2.zero()
 
 
-    
     # continues to read encoder values for testing until "Ctrl-C" is pressed
+    
 while True:
     try:
         # run motor and read encoder vals
         # init motor time channels init-ed in motor_driver class
         
-        Kp = float(input("Enter the proportional gain (Kp) =  "))
-        setpoint = int(input("Enter the set-point =  "))
+        Kp = .5 #float(input("Enter the proportional gain (Kp) =  "))
+        setpoint = 6000 #int(input("Enter the set-point =  "))
         
-        controller_obj = Controller(Kp, setpoint)
-        controller_obj.run(enc2.read())
+        reader_value = enc2.read()
+
         
-        count = 0
-        
-        while: count 
-        
-        # Drive the motor
-        moe.set_duty_cycle (50)
-        
-        #enc1.read()
-        print('------')
-        enc2.read()
-        print('count',count)
-        print('------------------')
-        time.sleep(0.1)
+        while True:
+            
+            
+            controller_obj = Controller(Kp, setpoint)
+            PWM = controller_obj.run(reader_value)
+            actuate_motor = moe.set_duty_cycle(PWM)
+            utime.sleep_ms(5)
 
         
     # Trying to catch the "Ctrl-C" keystroke to break out
