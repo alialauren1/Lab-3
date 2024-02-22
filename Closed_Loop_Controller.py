fd345c620351728f2f66984237830da92962faf1
andyjwaideh <andyjwaideh@gmail.com>
2024-02-22 15:55:51 -0800

    Working Code

--- Closed_Loop_Controller.py
+++ Closed_Loop_Controller.py
@@ -2,12 +2,12 @@ import utime
 import cqueue
 
 class Controller:
-    def __init__(self, kp, set_point):
+    def __init__(self, kp, set_point, queue_size):
          self.kp = float(kp)
          self.set_point = float(set_point)
          #self.measured_output = 0
-         self.time_value = cqueue.IntQueue(1000)
-         self.position = cqueue.IntQueue(1000)
+         self.time_value = cqueue.IntQueue(queue_size)
+         self.position = cqueue.IntQueue(queue_size)
          self.start = utime.ticks_ms()
          
 
@@ -17,13 +17,12 @@ class Controller:
          err = self.set_point - self.measured_output
          actuation = self.kp*err
          current_time = utime.ticks_ms()
-         if not self.time_value.full():
-             self.time_passed = utime.ticks_diff(current_time, self.start)
-             self.time_value.put(self.time_passed)
-             self.position.put(self.measured_output)
-             return actuation
-         else:
-             return "done"
+         #if not self.time_value.full():
+         self.time_passed = utime.ticks_diff(current_time, self.start)
+         self.time_value.put(self.time_passed)
+         self.position.put(self.measured_output)
+         #utime.sleep_ms(10) # does this go in main or control?
+         return actuation
         
     def set_setpoint(self, desired_set_point):
         self.set_point = desired_set_point
@@ -31,10 +30,10 @@ class Controller:
     def set_Kp(self, desired_Kp):
         self.kp = desired_Kp
         
-    def data():
-        x = self.time_value.put(self.time_passed)
-        y = self.position.put(self.measured_output)
-        return x, y
+    def data(self):
+         x = self.time_value
+         y = self.position
+         return (x, y)
         
     
-    
+  # call .get on each one of the queues  
--- main.py
+++ main.py
@@ -8,28 +8,83 @@ from Closed_Loop_Controller import Controller
 enc2 = Encoder("enc2", pyb.Pin.board.PC6, pyb.Pin.board.PC7, 8)
 moe = motordriver (pyb.Pin.board.PC1, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
 enc2.zero()
+queue_size = 100
 
 
 
-Kp = .1 #float(input("Enter the proportional gain (Kp) =  "))
+Kp = .005 #float(input("Enter the proportional gain (Kp) =  "))
 setpoint = 50000 #int(input("Enter the set-point =  "))
-controller_obj = Controller(Kp, setpoint)
+controller_obj = Controller(Kp, setpoint, queue_size)
+
+for i in range(queue_size):
+    reader_value = enc2.read()
+    PWM = controller_obj.run(reader_value)
+    moe.set_duty_cycle(-PWM)
+    utime.sleep_ms(10)
+
+tup = controller_obj.data()
+time = tup[0]
+print(time)
+pos = tup[1]
+
+time_list = []
+pos_list =[]
+
+for i in range(queue_size):
+    time_list.append(time.get())
     
-while True:
-    try:        
-        while True:
-            reader_value = enc2.read()
-            PWM = controller_obj.run(reader_value)
-            if isinstance(PWM, str):
-                print("done")
-                break
-            else:
-                moe.set_duty_cycle(-PWM)
-       # print("data")
-       # print(controller_obj.data())
+for i in range(queue_size):
+    pos_list.append(pos.get())
+
+# this is to look nice
+for i in range(queue_size):
+    row = f"{time_list[i]}, {pos_list[i]}"
+    print(row)
+
+# this is indep time
+for i in range(queue_size):
+    row = f"{time_list[i]}"
+    print(row)
+
+# this is indep pos
+for i in range(queue_size):
+    row = f"{pos_list[i]}"
+    print(row)
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     
+# while True:
+#     try:        
+#         while True:
+#             reader_value = enc2.read()
+#             PWM = controller_obj.run(reader_value)
+#             if isinstance(PWM, str):
+#                 print("done")
+#                 break
+#             elif PWM == 0:
+#                 print("done super done")
+#                 break
+#             else:
+#                 moe.set_duty_cycle(-PWM)
+#                 print(reader_value)
+                
+        #print("data")
+        #print(controller_obj.data())
+     #   break
             
     # Trying to catch the "Ctrl-C" keystroke to break out
     # of the program cleanly
-    except KeyboardInterrupt:
-        break
+   # except KeyboardInterrupt:
+   #     break
     
