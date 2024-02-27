c5fef1040d61fa764e2a614ab84944434c7b4b67
evoeddie <157066050+evoeddie@users.noreply.github.com>
2024-02-26 18:04:47 -0800

    Update

--- src/Closed_Loop_Controller.py
+++ src/Closed_Loop_Controller.py
@@ -15,6 +15,7 @@ class Controller:
      
     """
     def __init__(self, kp, set_point, queue_size):
+        
         """!
         Initializes the Contoller class with setpoint and proportinal gain.
         
@@ -22,13 +23,12 @@ class Controller:
         @param set_point (float): The desired set point for the controller.
         @param queue_size (int): The maximum size of the queue.
         """
-
-         self.kp = float(kp)
-         self.set_point = float(set_point)
+        self.kp = float(kp)
+        self.set_point = float(set_point)
          #self.measured_output = 0
-         self.time_value = cqueue.IntQueue(queue_size)
-         self.position = cqueue.IntQueue(queue_size)
-         self.start = utime.ticks_ms()
+        self.time_value = cqueue.IntQueue(queue_size)
+        self.position = cqueue.IntQueue(queue_size)
+        self.start = utime.ticks_ms()
          
 
     def run(self, measured_output):
@@ -43,17 +43,17 @@ class Controller:
         
         @returns float: The actuation value computed by our controller
         """
-         #actuation value is Kp*(theta_set - enc2.read() value
-         self.measured_output = measured_output
-         err = self.set_point - self.measured_output
-         actuation = self.kp*err
-         current_time = utime.ticks_ms()
+        #actuation value is Kp*(theta_set - enc2.read() value
+        self.measured_output = measured_output
+        err = self.set_point - self.measured_output
+        actuation = self.kp*err
+        current_time = utime.ticks_ms()
          #if not self.time_value.full():
-         self.time_passed = utime.ticks_diff(current_time, self.start)
-         self.time_value.put(self.time_passed)
-         self.position.put(self.measured_output)
+        self.time_passed = utime.ticks_diff(current_time, self.start)
+        self.time_value.put(self.time_passed)
+        self.position.put(self.measured_output)
          #utime.sleep_ms(10) # does this go in main or control?
-         return actuation
+        return actuation
         
     def set_setpoint(self, desired_set_point):
         """!
@@ -78,9 +78,9 @@ class Controller:
         
         @param tuple: A tuple containing time and position queses.
         """
-         x = self.time_value
-         y = self.position
-         return (x, y)
-        
+        x = self.time_value
+        y = self.position
+        return (x, y)
+                                            
     
   # call .get on each one of the queues  
